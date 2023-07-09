7d38d8dba7cf84008df18064e13ef43f18bd1d5c
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2023-07-09 21:16:04 +0300

    Fixed crash when constraint is defined before adapter

--- kronos/src/main/java/com/ironsource/aura/kronos/config/constraint/Constraint.kt
+++ kronos/src/main/java/com/ironsource/aura/kronos/config/constraint/Constraint.kt
@@ -1,69 +1,79 @@
 package com.ironsource.aura.kronos.config.constraint
 
-import com.ironsource.aura.kronos.utils.toCached
 import com.ironsource.aura.dslint.annotations.DSLMandatory
 import com.ironsource.aura.dslint.annotations.DSLint
+import com.ironsource.aura.kronos.utils.toCached
 
 @DSLint
 interface Constraint<Test, Fallback> {
 
-    var fallbackToPrimitive: Test
-    var fallbackTo: Fallback
+	var fallbackToPrimitive: Test
+	var fallbackTo: Fallback
 
-    @DSLMandatory(group = "constraint",
-            message = "At least one constraint of type 'acceptIf' or 'denyIf' must be defined")
-    fun acceptIf(block: (Test) -> Boolean)
+	@DSLMandatory(
+		group = "constraint",
+		message = "At least one constraint of type 'acceptIf' or 'denyIf' must be defined"
+	)
+	fun acceptIf(block: (Test) -> Boolean)
 
-    @DSLMandatory(group = "constraint")
-    fun denyIf(block: (Test) -> Boolean)
+	@DSLMandatory(group = "constraint")
+	fun denyIf(block: (Test) -> Boolean)
 
-    fun fallbackToPrimitive(fallbackProvider: (Test) -> Test)
+	fun fallbackToPrimitive(fallbackProvider: (Test) -> Test)
 
-    fun fallbackTo(cache: Boolean = true,
-                   fallbackProvider: (Test) -> Fallback)
+	fun fallbackTo(
+		cache: Boolean = true,
+		fallbackProvider: (Test) -> Fallback
+	)
 }
 
-internal class ConstraintBuilder<Test, Fallback> private constructor(var name: String? = null,
-                                                                     private var adapter: (Test) -> Fallback) : Constraint<Test, Fallback> {
+internal class ConstraintBuilder<Test, Fallback> private constructor(
+	var name: String? = null,
+	private var adapter: () -> ((Test) -> Fallback)
+) : Constraint<Test, Fallback> {
 
-    var verifiers: MutableList<(Test) -> Boolean> = mutableListOf()
-    var fallbackProvider: ((Test) -> Fallback)? = null
+	var verifiers: MutableList<(Test) -> Boolean> = mutableListOf()
+	var fallbackProvider: ((Test) -> Fallback)? = null
 
-    companion object {
-        internal operator fun <T, S> invoke(name: String? = null,
-                                            adapter: (T) -> S,
-                                            block: ConstraintBuilder<T, S>.() -> Unit) =
-                ConstraintBuilder(name, adapter).apply(block)
-    }
+	companion object {
+		internal operator fun <T, S> invoke(
+			name: String? = null,
+			adapter: () -> ((T) -> S),
+			block: ConstraintBuilder<T, S>.() -> Unit
+		) =
+			ConstraintBuilder(name, adapter).apply(block)
+	}
 
-    override fun acceptIf(block: (Test) -> Boolean) {
-        verifiers.add(block)
-    }
+	override fun acceptIf(block: (Test) -> Boolean) {
+		verifiers.add(block)
+	}
 
-    override fun denyIf(block: (Test) -> Boolean) {
-        verifiers.add { !block(it) }
-    }
+	override fun denyIf(block: (Test) -> Boolean) {
+		verifiers.add { !block(it) }
+	}
 
-    override var fallbackToPrimitive: Test
-        @Deprecated("", level = DeprecationLevel.ERROR)
-        get() = throw UnsupportedOperationException()
-        set(value) {
-            fallbackToPrimitive { value }
-        }
+	override var fallbackToPrimitive: Test
+		@Deprecated("", level = DeprecationLevel.ERROR)
+		get() = throw UnsupportedOperationException()
+		set(value) {
+			fallbackToPrimitive { value }
+		}
 
-    override fun fallbackToPrimitive(fallbackProvider: (Test) -> Test) {
-        fallbackTo { adapter(fallbackProvider(it)) }
-    }
+	override fun fallbackToPrimitive(fallbackProvider: (Test) -> Test) {
+		fallbackTo { adapter()(fallbackProvider(it)) }
+	}
 
-    override var fallbackTo: Fallback
-        @Deprecated("", level = DeprecationLevel.ERROR)
-        get() = throw UnsupportedOperationException()
-        set(value) {
-            fallbackTo { value }
-        }
+	override var fallbackTo: Fallback
+		@Deprecated("", level = DeprecationLevel.ERROR)
+		get() = throw UnsupportedOperationException()
+		set(value) {
+			fallbackTo { value }
+		}
 
-    override fun fallbackTo(cache: Boolean,
-                            fallbackProvider: (Test) -> Fallback) {
-        this.fallbackProvider = if (cache) fallbackProvider.toCached() else fallbackProvider
-    }
+	override fun fallbackTo(
+		cache: Boolean,
+		fallbackProvider: (Test) -> Fallback
+	) {
+		this.fallbackProvider = if (cache) fallbackProvider.toCached() else fallbackProvider
+	}
 }
--- kronos/src/test/java/com/ironsource/aura/kronos/constraint/MinValueConstraintTest.kt
+++ kronos/src/test/java/com/ironsource/aura/kronos/constraint/MinValueConstraintTest.kt
@@ -6,6 +6,7 @@ import com.ironsource.aura.kronos.common.withRemoteMap
 import com.ironsource.aura.kronos.config.FeatureRemoteConfig
 import com.ironsource.aura.kronos.config.constraint.FallbackPolicy
 import com.ironsource.aura.kronos.config.constraint.minValue
+import com.ironsource.aura.kronos.config.type.adaptedLongConfig
 import com.ironsource.aura.kronos.config.type.floatConfig
 import com.ironsource.aura.kronos.config.type.intConfig
 import com.ironsource.aura.kronos.config.type.longConfig
@@ -15,110 +16,118 @@ import kotlin.test.assertEquals
 
 object MinValueConstraintTest : Spek(kronosTest {
 
-    class DefaultFallbackConfig : FeatureRemoteConfig by mapConfig() {
+	class DefaultFallbackConfig : FeatureRemoteConfig by mapConfig() {
 
-        val someInt by intConfig {
-            default = 3
-            minValue = 2
-            cached = false
-        }
-        val someLong by longConfig {
-            default = 3
-            minValue = 2
-            cached = false
-        }
-        val someFloat by floatConfig {
-            default = 3f
-            minValue = 2f
-            cached = false
-        }
-    }
+		val someInt by intConfig {
+			default = 3
+			minValue = 2
+		}
+		val someLong by longConfig {
+			default = 3
+			minValue = 2
+		}
+		val someFloat by floatConfig {
+			default = 3f
+			minValue = 2f
+		}
+		val someAdapted by adaptedLongConfig {
+			default = ""
+			minValue = 0
+			adapt {
+				get { it.toString() }
+			}
+		}
+	}
 
-    val defaultFallbackConfig = DefaultFallbackConfig()
+	val defaultFallbackConfig = DefaultFallbackConfig()
 
-    describe("Remote value smaller than minValue should fallback to default") {
-        beforeGroup {
-            withRemoteMap(
-                    "someInt" to 1,
-                    "someLong" to 1L,
-                    "someFloat" to 1f
-            )
-        }
+	describe("Remote value smaller than minValue should fallback to default") {
+		beforeGroup {
+			withRemoteMap(
+				"someInt" to 1,
+				"someLong" to 1L,
+				"someFloat" to 1f,
+				"someAdapted" to -1L
+			)
+		}
 
-        it("Should return default - intConfig") {
-            assertEquals(3, defaultFallbackConfig.someInt)
+		it("Should return default - intConfig") {
+			assertEquals(3, defaultFallbackConfig.someInt)
+		}
+		it("Should return default - longConfig") {
+			assertEquals(3L, defaultFallbackConfig.someLong)
+		}
+		it("Should return default - floatConfig") {
+			assertEquals(3f, defaultFallbackConfig.someFloat)
+		}
+        it("Should return default - adaptedConfig") {
+            assertEquals("", defaultFallbackConfig.someAdapted)
         }
-        it("Should return default - longConfig") {
-            assertEquals(3L, defaultFallbackConfig.someLong)
-        }
-        it("Should return default - floatConfig") {
-            assertEquals(3f, defaultFallbackConfig.someFloat)
-        }
-    }
+	}
 
-    describe("Remote value equal or greater than minValue should be used") {
-        beforeGroup {
-            withRemoteMap(
-                    "someInt" to 5,
-                    "someLong" to 5L,
-                    "someFloat" to 5f
-            )
-        }
+	describe("Remote value equal or greater than minValue should be used") {
+		beforeGroup {
+			withRemoteMap(
+				"someInt" to 5,
+				"someLong" to 5L,
+				"someFloat" to 5f
+			)
+		}
 
-        it("Should return remote value - intConfig") {
-            assertEquals(5, defaultFallbackConfig.someInt)
-        }
-        it("Should return remote value - longConfig") {
-            assertEquals(5L, defaultFallbackConfig.someLong)
-        }
-        it("Should return remote value - floatConfig") {
-            assertEquals(5f, defaultFallbackConfig.someFloat)
-        }
-    }
+		it("Should return remote value - intConfig") {
+			assertEquals(5, defaultFallbackConfig.someInt)
+		}
+		it("Should return remote value - longConfig") {
+			assertEquals(5L, defaultFallbackConfig.someLong)
+		}
+		it("Should return remote value - floatConfig") {
+			assertEquals(5f, defaultFallbackConfig.someFloat)
+		}
+	}
 
-    describe("Remote value smaller than minValue with range fallback should fall to it") {
-        class RangeFallbackConfig : FeatureRemoteConfig by mapConfig() {
-            val someInt by intConfig {
-                default = 3
-                minValue {
-                    value = 2
-                    fallbackPolicy = FallbackPolicy.RANGE
-                }
-            }
-            val someLong by longConfig {
-                default = 3
-                minValue {
-                    value = 2L
-                    fallbackPolicy = FallbackPolicy.RANGE
-                }
-            }
-            val someFloat by floatConfig {
-                default = 3f
-                minValue {
-                    value = 2f
-                    fallbackPolicy = FallbackPolicy.RANGE
-                }
-            }
-        }
+	describe("Remote value smaller than minValue with range fallback should fall to it") {
+		class RangeFallbackConfig : FeatureRemoteConfig by mapConfig() {
+			val someInt by intConfig {
+				default = 3
+				minValue {
+					value = 2
+					fallbackPolicy = FallbackPolicy.RANGE
+				}
+			}
+			val someLong by longConfig {
+				default = 3
+				minValue {
+					value = 2L
+					fallbackPolicy = FallbackPolicy.RANGE
+				}
+			}
+			val someFloat by floatConfig {
+				default = 3f
+				minValue {
+					value = 2f
+					fallbackPolicy = FallbackPolicy.RANGE
+				}
+			}
+		}
 
-        val rangeFallbackConfig = RangeFallbackConfig()
+		val rangeFallbackConfig = RangeFallbackConfig()
 
-        beforeGroup {
-            withRemoteMap(
-                    "someInt" to 1,
-                    "someLong" to 1L,
-                    "someFloat" to 1f
-            )
-        }
+		beforeGroup {
+			withRemoteMap(
+				"someInt" to 1,
+				"someLong" to 1L,
+				"someFloat" to 1f
+			)
+		}
 
-        it("Should return minValue - intConfig") {
-            assertEquals(2, rangeFallbackConfig.someInt)
-        }
-        it("Should return minValue - longConfig") {
-            assertEquals(2L, rangeFallbackConfig.someLong)
-        }
-        it("Should return minValue - floatConfig") {
-            assertEquals(2f, rangeFallbackConfig.someFloat)
-        }
-    }
+		it("Should return minValue - intConfig") {
+			assertEquals(2, rangeFallbackConfig.someInt)
+		}
+		it("Should return minValue - longConfig") {
+			assertEquals(2L, rangeFallbackConfig.someLong)
+		}
+		it("Should return minValue - floatConfig") {
+			assertEquals(2f, rangeFallbackConfig.someFloat)
+		}
+	}
 })
