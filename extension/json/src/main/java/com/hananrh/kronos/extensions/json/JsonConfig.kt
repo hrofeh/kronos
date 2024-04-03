fe2dffdc374384c26a72efc59308c51647af7930
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2024-04-03 17:15:10 +0300

    Json test

--- extension/json/src/main/java/com/hananrh/kronos/extensions/json/JsonConfig.kt
+++ extension/json/src/main/java/com/hananrh/kronos/extensions/json/JsonConfig.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UnusedReceiverParameter")
+
 package com.hananrh.kronos.extensions.json
 
 import com.hananrh.kronos.Kronos
@@ -5,6 +7,7 @@ import com.hananrh.kronos.config.Config
 import com.hananrh.kronos.config.ConfigPropertyFactory
 import com.hananrh.kronos.config.FeatureRemoteConfig
 import com.hananrh.kronos.config.SourceTypeResolver
+import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
 val kronosJsonSerializer
@@ -13,23 +16,29 @@ val kronosJsonSerializer
 inline fun <reified T> FeatureRemoteConfig.jsonConfig(noinline block: Config<String, T>.() -> Unit) = ConfigPropertyFactory.from(
 	SourceTypeResolver.string(),
 	validator = { it.isNotEmpty() },
-	getterAdapter = {
-		try {
-			kronosJsonSerializer.fromJson<T>(it, typeOf<T>())
-		} catch (e: Exception) {
-			Kronos.logger?.e("Failed to parse json: $it", e)
-			null
-		}
-	},
-	setterAdapter = { value ->
-		value?.let {
-			try {
-				kronosJsonSerializer.toJson(it, typeOf<T>())
-			} catch (e: Exception) {
-				Kronos.logger?.e("Failed to convert to json", e)
-				null
-			}
-		}
-	},
+	getterAdapter = JsonGetterAdapter(typeOf<T>()),
+	setterAdapter = JsonSetterAdapter(typeOf<T>()),
 	block = block
 )
+
+class JsonGetterAdapter<T>(private val type: KType) : (String) -> T? {
+
+	override fun invoke(json: String) = try {
+		kronosJsonSerializer.fromJson<T>(json, type)
+	} catch (e: Exception) {
+		Kronos.logger?.e("Failed to parse json: $json", e)
+		null
+	}
+}
+
+class JsonSetterAdapter<T>(private val type: KType) : (T) -> String? {
+
+	override fun invoke(value: T) = value?.let {
+		try {
+			kronosJsonSerializer.toJson(it, type)
+		} catch (e: Exception) {
+			Kronos.logger?.e("Failed to convert to json", e)
+			null
+		}
+	}
+}
