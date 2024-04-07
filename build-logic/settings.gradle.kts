0f1b3cb7f4d9194b5feabaa2ae028ac98cba21e8
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2024-04-07 11:19:36 +0300

    Changed version catalog file name to avoid confusion with app catalog

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
 	}
 	versionCatalogs {
 		create("libs") {
-			from(files("../gradle/libs.versions.toml"))
+			from(files("../gradle/kronos.versions.toml"))
 		}
 	}
 }
