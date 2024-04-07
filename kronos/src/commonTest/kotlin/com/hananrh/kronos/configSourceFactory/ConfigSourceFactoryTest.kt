d70bed8a548d9c26773f0d925ead8bab7be56aa4
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2024-04-07 15:45:52 +0300

    Converted tests to kotest cont.

--- kronos/src/commonTest/kotlin/com/hananrh/kronos/configSourceFactory/ConfigSourceFactoryTest.kt
+++ kronos/src/commonTest/kotlin/com/hananrh/kronos/configSourceFactory/ConfigSourceFactoryTest.kt
@@ -1,44 +1,39 @@
-//package com.hananrh.kronos.configSourceFactory
-//
-//import com.hananrh.kronos.Kronos
-//import com.hananrh.kronos.KronosConfig
-//import com.hananrh.kronos.common.ConsoleKronosLogger
-//import com.hananrh.kronos.common.MapSource
-//import com.hananrh.kronos.config.intConfig
-//import com.hananrh.kronos.source.identifiableTypedSource
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.spekframework.spek2.Spek
-//import org.spekframework.spek2.style.specification.describe
-//import kotlin.test.assertEquals
-//
-//object ConfigSourceFactoryTest : Spek({
-//
-//	beforeGroup {
-//		Kronos.init {
-//			logging {
-//				logger = ConsoleKronosLogger()
-//			}
-//			configSourceFactory<MapSource, String>(MapSource::class) {
-//				MapSource(it, mutableMapOf("prefixSomeInt" to 1))
-//			}
-//		}
-//	}
-//
-//	describe("Using config source with prefix") {
-//
-//		class Config : KronosConfig {
-//
-//			override val sourceDefinition = identifiableTypedSource<MapSource, String>("prefix")
-//
-//			val someInt by intConfig {
-//				default = 0
-//			}
-//		}
-//
-//		val config = Config()
-//
-//		it("Should return configured value with prefix") {
-//			assertEquals(1, config.someInt)
-//		}
-//	}
-//})
+package com.hananrh.kronos.configSourceFactory
+
+import com.hananrh.kronos.Kronos
+import com.hananrh.kronos.KronosConfig
+import com.hananrh.kronos.common.ConsoleKronosLogger
+import com.hananrh.kronos.common.MapSource
+import com.hananrh.kronos.config.intConfig
+import com.hananrh.kronos.source.identifiableTypedSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.equals.shouldBeEqual
+
+object ConfigSourceFactoryTest : FunSpec({
+
+	beforeSpec {
+		Kronos.init {
+			logging {
+				logger = ConsoleKronosLogger()
+			}
+			configSourceFactory<MapSource, String>(MapSource::class) {
+				MapSource(it, mutableMapOf("prefixSomeInt" to 1))
+			}
+		}
+	}
+
+	class Config : KronosConfig {
+
+		override val sourceDefinition = identifiableTypedSource<MapSource, String>("prefix")
+
+		val someInt by intConfig {
+			default = 0
+		}
+	}
+
+	val config = Config()
+
+	test("Should return configured value with prefix") {
+		config.someInt shouldBeEqual 1
+	}
+})
--- kronos/src/commonTest/kotlin/com/hananrh/kronos/configSourceFactory/ScopedSourceTest.kt
+++ kronos/src/commonTest/kotlin/com/hananrh/kronos/configSourceFactory/ScopedSourceTest.kt
@@ -1,33 +1,33 @@
-//package com.hananrh.kronos.configSourceFactory
-//
-//import com.hananrh.kronos.KronosConfig
-//import com.hananrh.kronos.common.MapSource
-//import com.hananrh.kronos.common.kronosTest
-//import com.hananrh.kronos.config.intConfig
-//import com.hananrh.kronos.source.scopedSource
-//import org.spekframework.spek2.Spek
-//import org.spekframework.spek2.style.specification.describe
-//import kotlin.test.assertEquals
-//
-//object ScopedSourceTest : Spek(kronosTest {
-//
-//	describe("Using config source with prefix") {
-//
-//		class Config : KronosConfig {
-//
-//			override val sourceDefinition = scopedSource(
-//				MapSource(map = mutableMapOf("someInt" to 1))
-//			)
-//
-//			val someInt by intConfig {
-//				default = 0
-//			}
-//		}
-//
-//		val config = Config()
-//
-//		it("Should return configured value with scoped source") {
-//			assertEquals(1, config.someInt)
-//		}
-//	}
-//})
+package com.hananrh.kronos.configSourceFactory
+
+import com.hananrh.kronos.KronosConfig
+import com.hananrh.kronos.common.MapSource
+import com.hananrh.kronos.common.initKronos
+import com.hananrh.kronos.config.intConfig
+import com.hananrh.kronos.source.scopedSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.equals.shouldBeEqual
+
+object ScopedSourceTest : FunSpec({
+
+	beforeSpec {
+		initKronos()
+	}
+
+	class Config : KronosConfig {
+
+		override val sourceDefinition = scopedSource(
+			MapSource(map = mutableMapOf("someInt" to 1))
+		)
+
+		val someInt by intConfig {
+			default = 0
+		}
+	}
+
+	val config = Config()
+
+	test("Should return configured value with scoped source") {
+		config.someInt shouldBeEqual 1
+	}
+})
