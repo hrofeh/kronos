90d8021b6e7ef2d3aeb8a75e57e4ad05b855e30e
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2024-04-07 15:43:11 +0300

    Converted tests to kotest cont.

--- kronos/src/commonTest/kotlin/com/hananrh/kronos/customConfigs/UserCustomConfigTest.kt
+++ kronos/src/commonTest/kotlin/com/hananrh/kronos/customConfigs/UserCustomConfigTest.kt
@@ -1,63 +1,59 @@
-//package com.hananrh.kronos.customConfigs
-//
-//import com.hananrh.kronos.common.kronosTest
-//import com.hananrh.kronos.common.mapConfig
-//import com.hananrh.kronos.common.withRemoteMap
-//import com.hananrh.kronos.config.Config
-//import com.hananrh.kronos.KronosConfig
-//import org.spekframework.spek2.Spek
-//import org.spekframework.spek2.style.specification.describe
-//import kotlin.test.assertEquals
-//
-//data class Label(val value: String)
-//
-//@Suppress("UnusedReceiverParameter")
-//fun KronosConfig.labelConfig(block: Config<String, Label>.() -> Unit) =
-//	com.hananrh.kronos.config.ConfigPropertyFactory.from(
-//		com.hananrh.kronos.config.ConfigSourceResolver.String,
-//		validator = { it.isNotEmpty() },
-//		getterAdapter = { Label(it) },
-//		setterAdapter = { it.value },
-//		block = block
-//	)
-//
-//object UserCustomConfigTest : Spek(kronosTest {
-//
-//	class Config : KronosConfig by mapConfig() {
-//
-//		var someLabel by labelConfig {
-//			default = Label("default")
-//			cached = false
-//		}
-//	}
-//
-//	val config = Config()
-//
-//	describe("Enum config field get") {
-//
-//		it("Should return remote value when valid") {
-//			withRemoteMap(
-//				"someLabel" to "remote"
-//			)
-//
-//			assertEquals(Label("remote"), config.someLabel)
-//		}
-//
-//		it("Should return default value when invalid") {
-//			withRemoteMap(
-//				"someLabel" to ""
-//			)
-//
-//			assertEquals(Label("default"), config.someLabel)
-//		}
-//	}
-//
-//	describe("Enum config field set") {
-//
-//		it("Should return set value") {
-//			config.someLabel = Label("override")
-//			assertEquals(Label("override"), config.someLabel)
-//		}
-//	}
-//})
-//
+package com.hananrh.kronos.customConfigs
+
+import com.hananrh.kronos.KronosConfig
+import com.hananrh.kronos.common.initKronos
+import com.hananrh.kronos.common.mapConfig
+import com.hananrh.kronos.common.withRemoteMap
+import com.hananrh.kronos.config.Config
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.equals.shouldBeEqual
+
+private data class Label(val value: String)
+
+@Suppress("UnusedReceiverParameter")
+private fun KronosConfig.labelConfig(block: Config<String, Label>.() -> Unit) =
+	com.hananrh.kronos.config.ConfigPropertyFactory.from(
+		com.hananrh.kronos.config.ConfigSourceResolver.String,
+		validator = { it.isNotEmpty() },
+		getterAdapter = { Label(it) },
+		setterAdapter = { it.value },
+		block = block
+	)
+
+object UserCustomConfigTest : FunSpec({
+
+	beforeSpec {
+		initKronos()
+	}
+
+	class Config : KronosConfig by mapConfig() {
+
+		var someLabel by labelConfig {
+			default = Label("default")
+			cached = false
+		}
+	}
+
+	val config = Config()
+
+	test("Should return remote value when valid") {
+		withRemoteMap(
+			"someLabel" to "remote"
+		)
+
+		config.someLabel shouldBeEqual Label("remote")
+	}
+
+	test("Should return default value when invalid") {
+		withRemoteMap(
+			"someLabel" to ""
+		)
+
+		config.someLabel shouldBeEqual Label("default")
+	}
+
+	test("Should return set value") {
+		config.someLabel = Label("override")
+		config.someLabel shouldBeEqual Label("override")
+	}
+})
