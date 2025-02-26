3d98d13860f73279072e0e08631713d8e91e85a0
hanan.rofehaim <hanan.rofehaim@unity3d.com>
2025-02-26 15:03:36 +0200

    Bumped to 1.2.3. Updated DSLint to 1.1.2. Updated Kotlin to 1.9.23. Updated AGP to 8.7.3. Updated gradle. Create sample module. Using ResourcesCompat for float defaultRes.

--- kronos/src/main/java/com/hananrh/kronos/config/ResourcesResolver.kt
+++ kronos/src/main/java/com/hananrh/kronos/config/ResourcesResolver.kt
@@ -1,21 +1,22 @@
 package com.hananrh.kronos.config
 
 import android.content.res.Resources
+import androidx.core.content.res.ResourcesCompat
 import com.hananrh.kronos.utils.getAny
 import com.hananrh.kronos.utils.getLong
 import com.hananrh.kronos.utils.getStringSet
 
 data class ResourcesResolver<T>(
-	val resourcesGetter: Resources.(Int) -> T
+    val resourcesGetter: Resources.(Int) -> T
 ) {
 
-	companion object {
-		val Int = ResourcesResolver(Resources::getInteger)
-		val Long = ResourcesResolver(Resources::getLong)
-		val Float = ResourcesResolver(Resources::getFloat)
-		val Boolean = ResourcesResolver(Resources::getBoolean)
-		val String = ResourcesResolver(Resources::getString)
-		val StringSet = ResourcesResolver(Resources::getStringSet)
-		val Any = ResourcesResolver(Resources::getAny)
-	}
+    companion object {
+        val Int = ResourcesResolver(Resources::getInteger)
+        val Long = ResourcesResolver(Resources::getLong)
+        val Float = ResourcesResolver { ResourcesCompat.getFloat(this, it) }
+        val Boolean = ResourcesResolver(Resources::getBoolean)
+        val String = ResourcesResolver(Resources::getString)
+        val StringSet = ResourcesResolver(Resources::getStringSet)
+        val Any = ResourcesResolver(Resources::getAny)
+    }
 }
