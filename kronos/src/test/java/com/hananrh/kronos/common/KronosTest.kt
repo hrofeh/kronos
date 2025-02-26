ee0f8cf9d1a572c938aa1f099bfd6929d6c1c1c7
hanan.rofehaim <hanan.rofehaim@unity3d.com>
2025-02-26 15:10:57 +0200

    Fixed unitests

--- kronos/src/test/java/com/hananrh/kronos/common/KronosTest.kt
+++ kronos/src/test/java/com/hananrh/kronos/common/KronosTest.kt
@@ -1,15 +1,21 @@
 package com.hananrh.kronos.common
 
+import androidx.core.content.res.ResourcesCompat
 import com.hananrh.kronos.Kronos
 import com.hananrh.kronos.config.FeatureRemoteConfig
 import com.hananrh.kronos.converter.kotlinx_serialization.KotlinxSerializationConverter
 import com.hananrh.kronos.source.typedSource
+import io.mockk.every
+import io.mockk.mockkStatic
 import org.spekframework.spek2.dsl.Root
 
 fun kronosTest(block: Root.() -> Unit): Root.() -> Unit {
 	return {
 
 		beforeGroup {
+			mockkStatic(ResourcesCompat::class)
+			every { ResourcesCompat.getFloat(any(),any()) } returns 0f
+
 			Kronos.init {
 				context = mockContext()
 				logging {
